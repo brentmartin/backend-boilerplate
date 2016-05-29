89851ee172f48b2c523eac8168e995493df28bfb
Brent Martin <bmart35@gmail.com>
2016-05-29 15:48:03 -0500

    put in javascript for navbar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,18 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+$(document).ready(function() {
+  var menuToggle = $("#js-navigation-centered-mobile-menu").unbind();
+  $("#js-navigation-centered-menu").removeClass("show");
+
+  menuToggle.on("click", function(e) {
+    e.preventDefault();
+    $("#js-navigation-centered-menu").slideToggle(function(){
+      if($("#js-navigation-centered-menu").is(":hidden")) {
+        $("#js-navigation-centered-menu").removeAttr("style");
+      }
+    });
+  });
+});
